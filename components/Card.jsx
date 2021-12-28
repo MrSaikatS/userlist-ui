0b8b3ac702f706bf426e31abd5d0a8d2ce66ab74
Saikat Sardar <saikatsardar5@gmail.com>
2021-12-28 21:08:45 +0530

    v3

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,79 +2,40 @@ import Image from "next/image";
 
 const Card = () => {
 	return (
-		<div className='max-w-sm mx-auto grid grid-flow-row border border-black py-4'>
-			<div className='grid grid-flow-col place-content-center'>
-				<div className=''>
-					<div className='w-32 h-32 rounded-full ring ring-offset-2'>
-						<Image
-							className='rounded-full mx-auto'
-							src='https://randomuser.me/api/portraits/women/10.jpg'
-							alt='user pic'
-							width={128}
-							height={128}
-						/>
-					</div>
+		<div className='w-96 h-auto text-lg rounded-xl shadow-xl shadow-purple-500/50 py-5 bg-gradient-to-b from-pink-500 to-purple-500'>
+			<div className='flex justify-center mb-5'>
+				<div className='w-32 h-32 self-center rounded-full border-2 border-violet-500'>
+					<Image
+						className='rounded-full'
+						src='https://randomuser.me/api/portraits/women/10.jpg'
+						alt='img'
+						width={128}
+						height={128}
+					/>
 				</div>
 			</div>
+			<div className='grid grid-flow-row mx-10 gap-x-4 text-white'>
+				<div className='grid grid-cols-4'>
+					<div className='justify-self-end'>Name : </div>
+					<div className='col-span-3 ml-4'>Mrs Hayley Davies</div>
+				</div>
+				<div className='grid grid-cols-4'>
+					<div className='justify-self-end'>Age : </div>
+					<div className='col-span-3 ml-4'>27</div>
+				</div>
+				<div className='grid grid-cols-4'>
+					<div className='justify-self-end'>Gender : </div>
+					<div className='col-span-3 ml-4'>Female</div>
+				</div>
 
-			<div className='text-lg place-items-center'>
-				<div className='grid grid-cols-4 gap-2'>
-					<div className='justify-self-end'>
-						<Image
-							className='self-center'
-							src='/icons8-user-96.png'
-							alt='user pic'
-							width={26}
-							height={26}
-						/>
-					</div>
-					<div className='col-span-3'>Lucy Phillips</div>
+				<div className='grid grid-cols-4'>
+					<div className='justify-self-end'>Email : </div>
+					<div className='col-span-3 ml-4'>hayley.davies@example.com</div>
 				</div>
-				<div className='grid grid-cols-4 gap-2'>
-					<div className='justify-self-end'>
-						<Image
-							className='self-center'
-							src='/icons8-at-sign-96.png'
-							alt='user pic'
-							width={26}
-							height={26}
-						/>
-					</div>
-					<div className='col-span-3'>lucy.phillips@example.com</div>
-				</div>
-				<div className='grid grid-cols-4 gap-2'>
-					<div className='justify-self-end'>
-						<Image
-							className=''
-							src='/icons8-gender-96.png'
-							alt='user pic'
-							width={26}
-							height={26}
-						/>
-					</div>
-					<div className='capitalize'>female</div>
-					<div className='justify-self-end'>
-						<Image
-							className=''
-							src='/icons8-circle-96.png'
-							alt='user pic'
-							width={26}
-							height={26}
-						/>
-					</div>
-					<div className=''>33</div>
-				</div>
-				<div className='grid grid-cols-4 gap-2'>
-					<div className='justify-self-end'>
-						<Image
-							className=''
-							src='/icons8-address-96.png'
-							alt='user pic'
-							width={26}
-							height={26}
-						/>
-					</div>
-					<div className='col-span-3'>6640, Stanley Road. Dundee.</div>
+
+				<div className='grid grid-cols-4'>
+					<div className='justify-self-end'>Address : </div>
+					<div className='col-span-3 ml-4'>priya@gmail.com</div>
 				</div>
 			</div>
 		</div>
@@ -82,3 +43,5 @@ const Card = () => {
 };
 
 export default Card;
+
+//https://randomuser.me/api/portraits/women/10.jpg
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,14 +2,17 @@ import Card from "../components/Card";
 
 const index = () => {
 	return (
-		<div className='container mx-auto'>
-			<div className='text-center'>UserList UI</div>
-			<div className='grid grid-cols-3 '>
+		<>
+			<div className='text-center text-xl my-5'>User List UI Design</div>
+			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 grid-flow-row place-items-center gap-20'>
+				<Card />
+				<Card />
+				<Card />
 				<Card />
 				<Card />
 				<Card />
 			</div>
-		</div>
+		</>
 	);
 };
 
