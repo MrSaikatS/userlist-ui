fe93d69df76f61465e3b0caee907cb3fab069710
syedhossain891 <94627023+syedhossain891@users.noreply.github.com>
2021-12-30 22:00:30 +0530

    m

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,30 @@
 import Card from "../components/Card";
 
 const index = () => {
-	return (
-		<>
-			<div className='mx-8 rounded-3xl mt-6 mb-10 shadow-lg bg-blue-900 shadow-blue-900/50'>
-				<h1 className='text-center text-white text-xl py-4'>
-					User List UI Design
-				</h1>
-			</div>
+  return (
+    <>
+      <div className='mx-8 rounded-3xl mt-6 mb-10 shadow-lg bg-blue-900 shadow-blue-900/50'>
+        <h1 className='text-center text-white text-xl py-4'>
+          User List UI Design
+        </h1>
+      </div>
 
-			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 grid-flow-row place-items-center gap-20'>
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-			</div>
-		</>
-	);
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 grid-flow-row place-items-center gap-20'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
+  );
 };
+
+export async function getServerSideProps() {
+  const req = await fetch(``);
+}
 
 export default index;
