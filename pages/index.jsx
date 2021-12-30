3d95529f212a3983bcb652946043560aecae7ae7
Saikat Sardar <saikatsardar5@gmail.com>
2021-12-30 22:06:45 +0530

    V6 Final + Fixes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,32 +1,5 @@
 import Card from "../components/Card";
 
-<<<<<<< Updated upstream
-const index = () => {
-  return (
-    <>
-      <div className='mx-8 rounded-3xl mt-6 mb-10 shadow-lg bg-blue-900 shadow-blue-900/50'>
-        <h1 className='text-center text-white text-xl py-4'>
-          User List UI Design
-        </h1>
-      </div>
-
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 grid-flow-row place-items-center gap-20'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    </>
-  );
-};
-
-export async function getServerSideProps() {
-  const req = await fetch(``);
-=======
 const index = (props) => {
 	const data = props.res;
 
@@ -55,7 +28,6 @@ export async function getServerSideProps() {
 	return {
 		props: { res },
 	};
->>>>>>> Stashed changes
 }
 
 export default index;
